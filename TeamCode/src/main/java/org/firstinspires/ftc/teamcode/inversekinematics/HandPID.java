59164428ad443afe925dd08fcda957d074e8f53a
Elior Yousefi <elioryous@gmail.com>
2023-11-01 13:10:21 +0200

    added pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/inversekinematics/HandPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/inversekinematics/HandPID.java
@@ -1,25 +1,28 @@
 package org.firstinspires.ftc.teamcode.inversekinematics;
 
+import com.ThermalEquilibrium.homeostasis.Controllers.Feedback.BasicPID;
+import com.ThermalEquilibrium.homeostasis.Parameters.PIDCoefficients;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-import TrcCommonLib.trclib.TrcPurePursuitDrive;
-
 @Config
-public class Hand extends LinearOpMode {
+public class HandPID extends LinearOpMode {
     Interpolation interpX, interpZ;
     Servo servoHandRight, serverHandLeft;
     Servo servoClaw;
     public static int xTarget = 50, zTarget = 100;
+
+    public static double kPx = 0.0001, kIx = 0, kDx = 0;
+    public static double kPz = 0.0001, kIz = 0, kDz = 0;
     public static double upperLength = 50; // hand
     public static double lowerLength = 100; // claw
 
     double currentMillis, previousMillis;
 
-    float x,z;
+    double x = 0,z = 0;
 
     double shoulderAngle2a, shoulderAngle2aDegrees, shoulderAngle2, shoulderAngle2Degrees, shoulderPos2, z2, shoulderAngle1a;
     double shoulderAngle1, elbowAngle, shoulderAngleDegrees, elbowAngleDegrees, elbowPos, shoulderPos;
@@ -27,6 +30,10 @@ public class Hand extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         ElapsedTime opmodeRunTime = new ElapsedTime();
+        PIDCoefficients coefficientsX = new PIDCoefficients(kPx,kIx,kDx);
+        PIDCoefficients coefficientsZ = new PIDCoefficients(kPz,kIz,kDz);
+        BasicPID controllerX = new BasicPID(coefficientsX);
+        BasicPID controllerZ = new BasicPID(coefficientsZ);
 
         servoHandRight = hardwareMap.get(Servo.class, "sHR");
         serverHandLeft = hardwareMap.get(Servo.class, "sHL");
@@ -41,9 +48,8 @@ public class Hand extends LinearOpMode {
                 previousMillis = currentMillis;
 
 
-                // start interpolation
-                z = interpZ.go(zTarget, 1000);
-                x = interpX.go(xTarget, 1000);
+                z = controllerZ.calculate(zTarget, z);
+                x = controllerX.calculate(zTarget, x);
 
                 // *** Inverse Kinematics ***
                 // calculate modification to shoulder angle and arm length
